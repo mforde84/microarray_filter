754fb13d52680024e038f9667df246f002dd7f96
Martin Forde <mforde84@gmail.com>
2016-09-06 17:54:01 -0500

    Update microarray_filter.cpp

--- microarray_filter.cpp
+++ microarray_filter.cpp
@@ -10,49 +10,49 @@ using namespace std;
 struct meta_frame{
 		
 	//object to store all input information
-    vector<unsigned int> control_index;
-    vector<vector<string>> probe_id;
-    vector<string> sample_id;
-    vector<vector<float>> data_frame;
+    	vector<unsigned int> control_index;
+    	vector<vector<string>> probe_id;
+    	vector<string> sample_id;
+	vector<vector<float>> data_frame;
 
 };
 
 vector<string> get_control_probes(char* input_file){
     
-    //gets line delimited list of probe ID stores in iteratable string vector
-    ifstream infile;
-    vector<string> pusher;
-    infile.open(input_file);
-    while (!infile.eof()){
-        string sLine = "";
-        getline(infile,sLine);
-        pusher.push_back(sLine);
-    }
-    infile.close();
+    	//gets line delimited list of probe ID stores in iteratable string vector
+    	ifstream infile;
+    	vector<string> pusher;
+	infile.open(input_file);
+    	while (!infile.eof()){
+		string sLine = "";
+		getline(infile,sLine);
+		pusher.push_back(sLine);
+    	}
+    	infile.close();
 	return pusher;
-	
+
 }
 
 meta_frame get_data(char* input_file, vector<string> control_probes){
     
 	//opens file with log2 signal intensities and stores in struct object
-    int line_count = 0;
+    	int line_count = 0;
 	meta_frame pusher;
-    ifstream infile;
-    infile.open(input_file);
-    while (!infile.eof()){
+    	ifstream infile;
+    	infile.open(input_file);
+    	while (!infile.eof()){
 		//opens the file, and iterates and tokenizes lines
-        string sLine, token;
-        vector<string> string_holder;
-        getline(infile,sLine);
-        istringstream iss(sLine);
-        while(getline(iss,token,'\t'))
-            string_holder.push_back(token);
-        //stores headers, i.e., sample id
-        if (line_count == 0){
-            for (x : string_holder)
-                pusher.sample_id.push_back(x);
-        }else{
+        	string sLine, token;
+        	vector<string> string_holder;
+        	getline(infile,sLine);
+        	istringstream iss(sLine);
+        	while(getline(iss,token,'\t'))
+            		string_holder.push_back(token);
+        	//stores headers, i.e., sample id
+		if (line_count == 0){
+            		for (x : string_holder)
+                		pusher.sample_id.push_back(x);
+        	}else{
 			//stores probe identifiers, and floats data
 			if (string_holder.size() != 0){
 				vector<string> holder;
@@ -66,45 +66,44 @@ meta_frame get_data(char* input_file, vector<string> control_probes){
 				//checks if probe id for the current line is a control probe
 				//if yes, stores the 0 index location of probe in the data matrix
 				for (x : control_probes){
-					if (x == string_holder[0]){
+					if (x == string_holder[0])
 						pusher.control_index.push_back(line_count - 1);
-					}
 				}
 			}
-   		}
-        line_count++;
-    }
-    infile.close();
-    return pusher;
+        	}
+        	line_count++;
+	}
+    	infile.close();
+    	return pusher;
 }
 
 vector<float> passfilter(meta_frame &input_data){
     
-    //determines the passing filter for each sample
-    vector<float> filter; 
-    //for each sample collects the control probe signal intensities then calculates 95 percentile
-    //stores result in vector 0-indexed by sample id
-    for (int y = 0; y < input_data.data_frame.at(0).size(); y++){
-        vector<float> holder;
-        for (z : input_data.control_index)
-            holder.push_back(input_data.data_frame[z][y]);
-        float min = *std::min_element(holder.begin(), holder.end());
-        float max = *std::max_element(holder.begin(), holder.end());
-        filter.push_back((0.95 * (max - min)) + min);
-    }
+    	//determines the passing filter for each sample
+    	vector<float> filter; 
+    	//for each sample collects the control probe signal intensities then calculates 95 percentile
+    	//stores result in vector 0-indexed by sample id
+    	for (int y = 0; y < input_data.data_frame.at(0).size(); y++){
+		vector<float> holder;
+        	for (z : input_data.control_index)
+            		holder.push_back(input_data.data_frame[z][y]);
+        	float min = *std::min_element(holder.begin(), holder.end());
+        	float max = *std::max_element(holder.begin(), holder.end());
+        	filter.push_back((0.95 * (max - min)) + min);
+    	}
 	return filter;
 	
 }
 
 void print_data(meta_frame &input_data, vector<float> filter){
     
-    //prints headers
-    for (x : input_data.sample_id)
+    	//prints headers
+    	for (x : input_data.sample_id)
 		cout << x << "\t";
 	cout << "\n";
     
-    //iterates through rows of data and determines if each data point passes for each respective sample filter 
-    for (int x = 0; x < input_data.data_frame.size(); x++){
+	//iterates through rows of data and determines if each data point passes for each respective sample filter 
+    	for (int x = 0; x < input_data.data_frame.size(); x++){
 		//prints probe information
 		cout << input_data.probe_id[x][0] << "\t" << input_data.probe_id[x][1] << "\t";
 		for (int y = 0; y < input_data.data_frame.at(x).size(); y++){
@@ -112,15 +111,16 @@ void print_data(meta_frame &input_data, vector<float> filter){
 			if (input_data.data_frame[x][y] > filter[y]){		
 				//if greater than filter by more than 1
 				if (input_data.data_frame[x][y] > (filter[y] + 1))
-                    cout << "P" << "\t";
-                //if only marginally greater
-                else
-                    cout << "M" << "\t";
-            }else //if not greater than filter
-                cout << "F" << "\t";
+                			cout << "P" << "\t";
+                		//if only marginally greater
+                		else
+                    			cout << "M" << "\t";
+        		}else //if not greater than filter
+                		cout << "F" << "\t";
 		}
-		cout << "\n";
+	cout << "\n";
 	}
+	
 }
 
 int main(int argc, char* argv[]){
@@ -128,16 +128,16 @@ int main(int argc, char* argv[]){
 	//usage ./microarray_filter log2_signal_intensities.txt control_probe_list.txt
 	
 	//load control probes
-    vector<string> control_list = get_control_probes(argv[2]);
+    	vector<string> control_list = get_control_probes(argv[2]);
     
-    //import data
-    meta_frame data = get_data(argv[1], control_list);
+    	//import data
+    	meta_frame data = get_data(argv[1], control_list);
     
-    //determine pass filter for each sample
-    vector<float> filter = passfilter(std::ref(data));
+    	//determine pass filter for each sample
+    	vector<float> filter = passfilter(std::ref(data));
     
-    //output results to STOUT
-    print_data(std::ref(data), filter);
+    	//output results to STOUT
+	print_data(std::ref(data), filter);
     
 	return 0;
 
